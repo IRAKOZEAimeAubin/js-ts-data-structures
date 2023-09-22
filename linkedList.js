609f9a7d8bc14b1095af04c3e9507e19f11a949b
Irakoze Aime Aubin <irakozeaimeaubin@gmail.com>
2023-09-22 15:45:28 +0200

    cleanup

---
+++ linkedList.js
@@ -0,0 +1,12 @@
+class Node {
+    constructor ( data ) {
+        this.data = data;
+        this.next = null;
+    }
+
+    setNextNode ( node ) {
+        if ( node instanceof Node || node === null ) {
+            
+        }
+    }
+}
